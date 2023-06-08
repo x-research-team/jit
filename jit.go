7ae209e8f0a18211815f1153756df6d22eb4632b
Adel Urazov <adel.i.urazov@ya.ru>
2023-06-08 15:44:13 +0300

    * feat(jit.go): add support for float32, float64, string and interface types * feat(jit.go): add ReflectTypeToType function to convert reflect.Type to jit.Type

--- jit.go
+++ jit.go
@@ -1,9 +1,19 @@
 package jit
 
 import (
+	"reflect"
+
 	"github.com/x-research-team/jit/internal/ccall"
 )
 
 var (
-	TypeInt = &Type{ccall.TypeGoInt}
+	TypeInt       = &Type{ccall.TypeGoInt}
+	TypeFloat32   = &Type{ccall.TypeFloat32}
+	TypeFloat64   = &Type{ccall.TypeFloat64}
+	TypeString    = &Type{ccall.TypeGoString}
+	TypeInterface = &Type{ccall.TypeGoInterface}
 )
+
+func ReflectTypeToType(t reflect.Type) *Type {
+	return &Type{ccall.ReflectTypeToType(t)}
+}
